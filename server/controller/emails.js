134ca67ea74aaad6cfb2d1a255402a4d99404b82
Joseph <jostheblessing@gmail.com>
2019-03-19 20:21:24 +0200

    [Feature 164767340]allow-user-a-logged-in-user-to-get-all-received-emails

---
+++ server/controller/emails.js
@@ -0,0 +1,16 @@
+import pool from "../config/connection";
+exports.getEmails = (req, res)=>{
+    const sql = "SELECT * FROM messages WHERE receiver_id=$1";
+    pool.query(sql,[req.user.id])
+     .then(messages =>{
+         // Check if there is any email
+         if(messages.rows.length === 0){
+             return res.status(404).json({notFound:"sorry there no messages"});
+         }
+         return res.status(200).json({ messages:messages.rows });
+     })
+     .catch(error=>{
+        //  console.log(error);
+      return res.status(500).json({error});
+     })
+}
